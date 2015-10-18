4e45f4050333abf31734a42521ee5c7bf274411b
georgepsarakis <giwrgos.psarakis@gmail.com>
2015-10-18 21:04:55 +0300

    BasePlugin class and common classes

---
+++ pitch/plugins/common.py
@@ -0,0 +1,71 @@
+from __future__ import unicode_literals
+import logging
+import time
+from ..lib.common.structures import PitchDict
+from ..lib.templating.structures import JinjaExpressionResolver
+
+
+class BasePlugin(object):
+    _name = None
+    _result = None
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def phase(self):
+        return self._name
+
+    @property
+    def result(self):
+        return self._result
+
+    @classmethod
+    def get_name(cls):
+        return cls._name
+
+    def execute(self, plugin_context):
+        pass
+
+
+class LoggerPlugin(BasePlugin):
+    _name = 'logger'
+
+    def __init__(self, logger_name=None, message=None, **kwargs):
+        if logger_name is None:
+            logger_name = 'logger.plugin'
+        logger_name = 'pitch.{}'.format(logger_name)
+        self.logger = logging.getLogger(logger_name)
+        self.logger.setLevel(logging.INFO)
+        handler = logging.FileHandler(**kwargs.get('handler', {}))
+        formatter_kwargs = kwargs.get('formatter', {})
+        formatter_kwargs['fmt'] = formatter_kwargs.get(
+            'fmt',
+            '%(asctime)s\t%(levelname)s\t%(message)s'
+        )
+        formatter = logging.Formatter(**formatter_kwargs)
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+        self._message = message
+
+    def execute(self, plugin_context):
+        self.logger.info(plugin_context.renderer(self._message))
+
+
+class DelayPlugin(BasePlugin):
+    def __init__(self, seconds):
+        self._delay_seconds = float(seconds)
+
+    def execute(self, plugin_context):
+        time.sleep(self._delay_seconds)
+
+
+class UpdateContext(BasePlugin):
+    def __init__(self, **updates):
+        self._updates = PitchDict(updates)
+
+    def execute(self, plugin_context):
+        interpreter = JinjaExpressionResolver(plugin_context)
+        for key, value in self._updates.iteritems():
+            plugin_context.renderer.context[key] = interpreter(value)
